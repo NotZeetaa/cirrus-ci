bc1da441db1bf942654eb0ac93851a23fea109b0
NotZeetaa <67799176+NotZeetaa@users.noreply.github.com>
2023-09-24 10:55:53 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Clone kernel
-git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b schedule kernel
+git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b sched-staging kernel
 
 # Build
 cd kernel
