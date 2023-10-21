219939b595b346d77e62692178fe975e1cbf92b0
NotZeetaa <67799176+NotZeetaa@users.noreply.github.com>
2023-10-21 10:46:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Clone kernel
-git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b rebase-new kernel
+git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b picel-sched kernel
 
 # Build
 cd kernel
@@ -9,4 +9,6 @@ bash build.sh null alioth lto
 bash build.sh null munch lto
 bash build.sh null lmi lto
 bash build.sh null apollo lto
+bash build.sh null cmi lto
+bash build.sh null umi lto
 
