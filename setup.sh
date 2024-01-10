1df14cbf4807e921a4aae0f9d185df42b4bf45a1
NotZeetaa <rodrigo2005contente@gmail.com>
2024-01-10 20:56:58 +0000

    Automatic run
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Clone kernel
-git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b latency-test kernel
+git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b rcu-test2 kernel
 
 # Build
 cd kernel
