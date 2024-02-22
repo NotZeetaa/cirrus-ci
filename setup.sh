dc793341d253aa1fc073dbc468120c46a71b7a09
NotZeetaa <rodrigo2005contente@gmail.com>
2024-02-22 19:44:40 +0000

    LTO build
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Clone kernel
-git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b rebase-3 kernel
+git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b rebase-4 kernel
 
 # Build
 cd kernel
-bash build.sh null alioth null
+bash build.sh null alioth lto
 
 
 
