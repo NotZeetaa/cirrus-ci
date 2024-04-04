14282b6cf6992ce3750b843b012f133bd8fd04c2
NotZeetaa <rodrigo2005contente@gmail.com>
2024-04-04 05:10:53 +0000

    Normal build
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Clone kernel
-git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b memory-test kernel
+git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b gcc-2 kernel
 
 # Build
 cd kernel
-bash build.sh null munch lto
+bash build.sh null munch null
 
 
