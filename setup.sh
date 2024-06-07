7abed0fa05354be99a0c0723877c7635d05d2c25
NotZeetaa <rodrigo2005contente@gmail.com>
2024-06-07 14:28:22 +0000

    Normal build
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Clone kernel
-git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b sched-9 kernel
+git clone --depth=1 https://github.com/projects-nexus/nexus_kernel_xiaomi_sm8250 -b rebase-sched-9 kernel
 
 # Build
 cd kernel
-bash build.sh null cmi lto
+bash build.sh null cmi null
 
 
 
